d5e6659781c4d1a2e7bee93f200a1ac0a1913d24
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-12-04 19:22:39 -0500

    Refactor game power calculation and add debug prints

--- day02/part02.py
+++ day02/part02.py
@@ -4,21 +4,6 @@ import re
 from pathlib import Path
 
 
-# these are the break conditions
-m = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
-
-
-def get_game_id(data: str) -> int:
-    """ extracts game id from the string """
-    pattern = r'\b(\d+)\b'
-    match = re.search(pattern, data.split(":")[0])
-    return int(match.group(1))
-
-
 def get_game_data(data: str) -> list[tuple[str, int]]:
     pattern = r'(\d+)\s+(\w+)'
     # Find all matches in the input string
@@ -27,21 +12,31 @@ def get_game_data(data: str) -> list[tuple[str, int]]:
     return [(match[1], int(match[0])) for match in matches]    
 
 
+def compute_cube_power(data: list[tuple[int, str]]) -> int:
+    """finds the max cube value for a game by color for a single game"""
+    power = 1
+    for color in ['red', 'green', 'blue']:
+        vals = []
+        for dat in data:
+            x, y = dat
+            if x == color:
+                vals.append(y)
+        power *= max(vals)
+            
+    return power
+        
+
 def main():
     with open(os.path.join(Path(__file__).parent, 'input.txt')) as f:
         data = [line.strip() for line in f.readlines()]
     
     sum = 0
-    for line in data:
-        game_id = get_game_id(line)
+    for index, line in enumerate(data, start=1):
         game_data = get_game_data(line)
-        
-        if not any([gdat[1] > m.get(gdat[0]) for gdat in game_data]):
-            print(f'Adding Game ID: {game_id}')
-            sum += game_id
-        else:
-            print(f"Game {game_id} not valid")
-    print(f'Total: {sum}')
+        power_value = compute_cube_power(game_data)
+        print(f"Game {index} Power Value: {power_value}")
+        sum += power_value
+    print(f"Sum of the power sets: {sum}")
 
 
 if __name__ == '__main__':
